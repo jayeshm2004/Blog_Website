b491e3d9701d58e0721739f24beed324b434d9c0
Jayesh Mishra <jayesh2004mishra@gmail.com>
2025-06-26 22:42:17 +0530

    commit on 26 june

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,DetailView,CreateView
+    )
 
 def home(request):
     context={
@@ -11,6 +13,20 @@ def home(request):
 class PostListView(ListView):
     model=Post
     template_name='blog/home.html'         #<app>/<model>_<viewtype>.html
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+class PostDetailView(DetailView):
+    model=Post
+
+class PostCreateView(CreateView):
+    model=Post
+    fields=['title','content']
+
+    def form_valid(self, form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request,'blog/about.html',{'title':'About'})
