92865c2f9ea94c53de8b1fbfe611830474d817cc
Jayesh Mishra <jayesh2004mishra@gmail.com>
2025-06-28 16:13:30 +0530

    commit before deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -135,3 +135,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL='login'
+
+
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+
+EMAIL_PORT=587
+
+EMAIL_USE_TLS=True
+
+EMAIL_HOST_USER='jayesh2004mishra@gmail.com'
+EMAIL_HOST_PASSWORD='tluj kkzb uvmz wltx'
